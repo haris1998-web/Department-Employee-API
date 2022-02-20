91b8a3221f55d3a643fa56ef175f694ad9077107
Muhammad Haris Yousaf <muhammadharisyousaf@gmail.com>
2022-02-20 11:35:34 +0500

    Update models.py

--- employeeapp/models.py
+++ employeeapp/models.py
@@ -4,9 +4,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Department(models.Model):
     DepartmentID = models.AutoField(primary_key=True)
     DepartmentName = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.DepartmentName
+
 
 class Employee(models.Model):
     EmployeeID = models.AutoField(primary_key=True)
@@ -14,3 +19,6 @@ class Employee(models.Model):
     Department = models.CharField(max_length=100)
     DateofJoining = models.DateField(auto_now=True)
     PhotoFileName = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.EmployeeName
